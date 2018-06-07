b613b6c1774ca414dc5a05494212efa14c9d7388
Yusuke Shibata <shibata@fata.io>
2018-06-07 18:18:53 +0900

    logo as png

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import logo from './logo.png'
 import gif from './video.gif'
 import styled, { injectGlobal } from 'styled-components'
 
